e8e16c0efe6eed3fa6fd82a6ce65a1c73fe691c3
JordanWilson <mrwizard6933@gmail.com>
2016-04-02 21:47:12 -0400

    set a new joystick variable

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
 	
-	var joystick;
+	var joystick = new VirtualJoystick();
 	
 	$("#play").click(function(){
 		
