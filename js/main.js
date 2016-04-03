3c7ff08206d1944caaac31e378f5d7a63aecd78c
JordanWilson <mrwizard6933@gmail.com>
2016-04-02 22:35:00 -0400

    changed the width and the height

--- js/main.js
+++ js/main.js
@@ -45,8 +45,8 @@ $(document).ready(function () {
 				mouseSupport	: true,
 				limitStickTravel: true,
 				stationaryBase: true,
-                      baseX: (window.innerWidth) * .80,
-                      baseY: (window.innerHeight) * .40,
+                      baseX: (window.innerWidth) * .90,
+                      baseY: (window.innerHeight) * .55,
 				stickRadius: 25
 			});
 		
