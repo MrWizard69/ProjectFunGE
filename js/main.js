809b1bafb1243319600bd1cb4cdb827f84ba7bfb
JordanWilson <mrwizard6933@gmail.com>
2016-05-21 16:47:38 -0400

    changed the ifs in the joystick update function

--- js/main.js
+++ js/main.js
@@ -136,23 +136,23 @@ $(document).ready(function () {
 			
 		for(var i = 0; i <= 4; i++){
 		
-			if (i == 1 && joystick.up()) {
+			if (joystick.up()) {
 					if (velY > -speed) {
 						velY--;
 					}
 				}
 
-				if (i == 2 && joystick.down()) {
+				if (joystick.down()) {
 					if (velY < speed) {
 						velY++;
 					}
 				}
-				if (i == 3 && joystick.right()) {
+				if (joystick.right()) {
 					if (velX < speed) {
 						velX++;
 					}
 				}
-				if (i == 4 && joystick.left()) {
+				if (joystick.left()) {
 					if (velX > -speed) {
 						velX--;
 					}
