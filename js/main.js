04820e67a11e31e7abd39b7eb295b56047b8b544
JordanWilson <mrwizard6933@gmail.com>
2016-05-21 20:07:41 -0400

    changing the joystick update function

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
 		
 		setInterval(function(){
 			
-			for(var i = 0; i <= 1; i++){
+			for(var i = 0; i <= 8; i++){
 				
 				if (joystick.up()) {
 						if (velY > -speed) {
