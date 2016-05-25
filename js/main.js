fd76b677541d27b012a3863e1347633433141637
JordanWilson <mrwizard6933@gmail.com>
2016-05-24 21:06:51 -0400

    made it less

--- js/main.js
+++ js/main.js
@@ -477,7 +477,7 @@ $(document).ready(function () {
 		if(canvas.width >= 241){
 			
 			joyStickX = (window.innerWidth) * .89;
-			joyStickY = (window.innerHeight) * .35;	
+			joyStickY = (window.innerHeight) * .20;	
 			
 			//$("#play").closest('.ui-btn').show();
 			
